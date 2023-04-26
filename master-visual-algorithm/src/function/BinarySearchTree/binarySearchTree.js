40606c54b57c0bd1617260000c673490d9ec3b53
a8901147 <a8901147@gmail.com>
2023-04-26 16:28:53 +0800

    f

--- master-visual-algorithm/src/function/BinarySearchTree/binarySearchTree.js
+++ master-visual-algorithm/src/function/BinarySearchTree/binarySearchTree.js
@@ -1,1 +1,14 @@
+//balanced tree
 //The absolute difference of heights of left and right subtrees at any node is less than 1
+
+// Binary search tree
+// Array Representation :
+// The binary tree can be represented using an array of size 2n+1 if the depth of the binary tree is n.
+// If the parent element is at the index p, Then the left child will be stored in the index 2p+1
+// and the right child will be stored in the index (2p)+2.
+
+//The array representation of the above binary tree is :
+//As in the above binary tree, A was the root node, so it will be stored in the 0th index.
+//The left child of A will be stored in the 2(0)+1 that is equal to the 1st location.
+//So, B is stored in index 1. And, similarly, the right child of A will be stored in the 2(0)+2 index.
+//For every node, the left and right child will be stored accordingly.
